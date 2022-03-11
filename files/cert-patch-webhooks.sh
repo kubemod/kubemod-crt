d557c506e52c09a8c5698a6d3145c1c2b46f15dc
Anthony Nash <anthony.nash@du.co>
2022-03-11 14:43:32 +0000

    Try apply not replace

--- files/cert-patch-webhooks.sh
+++ files/cert-patch-webhooks.sh
@@ -6,8 +6,8 @@ ca_bundle=$(cat /tmp/ca/ca.pem | base64 - | tr -d '\n' )
 echo "Applying mutating webhook configuration patch..."
 kubectl get mutatingwebhookconfiguration kubemod-mutating-webhook-configuration -o json | \
   jq -cr --arg caBundle $ca_bundle '.webhooks[].clientConfig.caBundle = $caBundle' | \
-  kubectl replace --force -f -
+  kubectl apply --record --force -f -
 echo "Applying validating webhook configuration patch..."
 kubectl get validatingwebhookconfiguration kubemod-validating-webhook-configuration -o json | \
   jq -cr --arg caBundle $ca_bundle '.webhooks[].clientConfig.caBundle = $caBundle' | \
-  kubectl replace --force -f -
+  kubectl apply --record --force -f -
