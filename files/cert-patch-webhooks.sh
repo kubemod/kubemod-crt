c0ed39668d52c24fb79c7f61b5fac9d956b8ad9d
Anthony Nash <anthony.nash@du.co>
2022-03-11 13:35:35 +0000

    Use correct path to ca.pem

--- files/cert-patch-webhooks.sh
+++ files/cert-patch-webhooks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-ca_bundle=$(cat ca.pem | base64 - | tr -d '\n' )
+ca_bundle=$(cat /tmp/ca/ca.pem | base64 - | tr -d '\n' )
 
 echo "Applying mutating webhook configuration patch..."
 kubectl get mutatingwebhookconfiguration kubemod-mutating-webhook-configuration -o json | \
